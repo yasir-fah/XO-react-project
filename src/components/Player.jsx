1b8e7e72eca22e87695af31df57e2aa984732080
YasirFah <yasiraltamimi178@gmail.com>
2025-06-03 18:05:33 +0300

    add functionality-name's button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [edit, setEdit] = useState(false);
+
+  function changeText() {
+    // setEdit(edit ? false : true); Same as:
+    setEdit(!edit);
+  }
+
+  // let btnCaption;
+  // if (edit === false) {
+  //   btnCaption = "Edit";
+  // } else {
+  //   btnCaption = "Save";
+  // }
   return (
     <>
       <li>
-        <span className="palyer">
+        {edit ? (
+          <span className="player">
+            <input type="text" required value={name} />
+          </span>
+        ) : (
           <span className="player-name">{name}</span>
-          <span className="player-symbol">{symbol}</span>
-        </span>
-        <button>Edit</button>
+        )}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={() => changeText()}>{!edit ? "Edit" : "Save"}</button>
       </li>
     </>
   );
